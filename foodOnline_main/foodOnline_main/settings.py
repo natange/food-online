bfea30375b11fcef9659a3bc9b28bed322661560
Jonatan Pacheco <yonh.guitar777@gmail.com>
2023-03-01 16:04:08 -0400

    template works ok

--- foodOnline_main/foodOnline_main/settings.py
+++ foodOnline_main/foodOnline_main/settings.py
@@ -9,7 +9,7 @@ https://docs.djangoproject.com/en/4.1/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.1/ref/settings/
 """
-
+import os.path
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -54,7 +54,7 @@ ROOT_URLCONF = 'foodOnline_main.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': ['templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -115,7 +115,11 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
+STATIC_ROOT = BASE_DIR /'static'
+STATICFILES_DIRS = [
+    BASE_DIR / 'foodOnline_main' / 'static'
+]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
--- foodOnline_main/foodOnline_main/views.py
+++ foodOnline_main/foodOnline_main/views.py
@@ -2,4 +2,5 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 def home(request):
-    return HttpResponse('Hola hijos de su republica madre')
+    #return HttpResponse('Hola hijos de su republica madre')
+    return render(request, 'home.html')
