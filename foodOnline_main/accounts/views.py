3b1319f4a5c42f3ec3e43741a13eda78ceccb3c9
Jonatan Pacheco <yonh.guitar777@gmail.com>
2023-03-10 04:04:32 -0400

    finish --> 9  ok.

--- foodOnline_main/accounts/views.py
+++ foodOnline_main/accounts/views.py
@@ -10,6 +10,7 @@ from django.core.exceptions import PermissionDenied
 from django.utils.http import urlsafe_base64_decode
 from django.contrib.auth.tokens import default_token_generator
 from django.db import models
+from vendor.models import Vendor
 
 
 # Create your views here.
