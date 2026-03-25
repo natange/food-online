d0ef253cfa1d37733170ed0cc214b7c002397e7b
natange <condoriyo@protonmail.com>
2026-03-25 00:12:07 -0400

    :construction: still working on user profile

--- src/core/urls.py
+++ src/core/urls.py
@@ -2,9 +2,11 @@ from django.contrib import admin
 from django.urls import path
 from django.urls import include
 from .views import home
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     path('', home, name='home'),
-    path('../accounts/', include('accounts.urls')),
+    path('accounts/', include('accounts.urls')),
     path('admin/', admin.site.urls),
-]
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
